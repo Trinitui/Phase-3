0c02afbee48128b3e42bff3d1c6a3a4885162e4e
Andrew Whelan <andrewmwhelan9@gmail.com>
2022-08-15 19:47:08 -0400

    docker files

---
+++ aw_create_tables.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS chicago_bi;
+CREATE DATABASE chicago_bi;
+
+DROP TABLE IF EXISTS MSDSCourseCatalog;
+
+\c msds;
+
+/*CREATE TABLE MSDSCourseCatalog (
+    CID VARCHAR(100),
+    CNAME VARCHAR(100) PRIMARY KEY,
+    CPREREQ VARCHAR(100)
+
+);
+
+\c msds;
+
+SELECT * FROM MSDSCourseCatalog;
+*/
