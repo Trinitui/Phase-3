891eb8638dda69c40a155c84dc812511b72780e0
Andrew Whelan <andrewmwhelan9@gmail.com>
2022-08-16 07:50:57 -0400

    port

--- run_microservices.go
+++ run_microservices.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"time"
 
 	"database/sql"
@@ -31,13 +32,44 @@ type CovidDataJsonRecords []struct {
 	Percent_tested_positive_weekly string `json:"percent_tested_positive_weekly"`
 }
 
+type BuildingPermitsJsonRecords []struct {
+	Permit_id         string `json:"id"`
+	Permit_issue_date string `json:"issue_date"`
+	Community_area    string `json:"community_area"`
+}
+
+type TaxiTripsJsonRecords []struct {
+	Trip_id                    string `json:"trip_id"`
+	Trip_start_timestamp       string `json:"trip_start_timestamp"`
+	Trip_end_timestamp         string `json:"trip_end_timestamp"`
+	Pickup_centroid_latitude   string `json:"pickup_centroid_latitude"`
+	Pickup_centroid_longitude  string `json:"pickup_centroid_longitude"`
+	Dropoff_centroid_latitude  string `json:"dropoff_centroid_latitude"`
+	Dropoff_centroid_longitude string `json:"dropoff_centroid_longitude"`
+}
+
+type TransportTripsJsonRecords []struct {
+	Trip_id                    string `json:"trip_id"`
+	Trip_start_timestamp       string `json:"trip_start_timestamp"`
+	Trip_end_timestamp         string `json:"trip_end_timestamp"`
+	Pickup_centroid_latitude   string `json:"pickup_centroid_latitude"`
+	Pickup_centroid_longitude  string `json:"pickup_centroid_longitude"`
+	Dropoff_centroid_latitude  string `json:"dropoff_centroid_latitude"`
+	Dropoff_centroid_longitude string `json:"dropoff_centroid_longitude"`
+}
+
+type UnemploymentJsonRecords []struct {
+	Community_area string `json:"community_area"`
+	Unemployment   string `json:"unemployment"`
+}
+
 func main() {
 	fmt.Println("Starting MicroServices!")
 	// Establish connection to Postgres Database
 	//db_connection := "user=postgres dbname=chicago_bi password=root host=localhost sslmode=disable"
 
 	// Docker image for the microservice - uncomment when deploy
-	db_connection := "user=postgres dbname=chicago_bi password=root host=8.8.8.8 sslmode=disable"
+	db_connection := "user=postgres dbname=chicago_bi password=root host=localhost port = 5433 sslmode=disable"
 
 	db, err := sql.Open("postgres", db_connection)
 	if err != nil {
@@ -61,6 +93,11 @@ func main() {
 		// This is a code snippet to show you how to pull data from different data sources.
 		GetCCVI(db)
 		GetCovidData(db)
+		GetBuildingPermits(db)
+		GetTaxiTrips(db)
+		GetTransportTrips(db)
+		GetUnemploymentData(db)
+
 		fmt.Println("Done!")
 		// Pull the data once a day
 		// You might need to pull Taxi Trips and COVID data on daily basis
@@ -292,5 +329,505 @@ func GetCovidData(db *sql.DB) {
 
 	}
 	fmt.Println("== Done with Covid Data ==")
+
+}
+
+func GetBuildingPermits(db *sql.DB) {
+
+	// This function is NOT complete
+	// It provides code-snippets for the data source: https://data.cityofchicago.org/Transportation/Taxi-Trips/wrvz-psew
+	// You need to complete the implmentation and add the data source: https://data.cityofchicago.org/Transportation/Transportation-Network-Providers-Trips/m6dm-c72p
+
+	// Data Collection needed from two data sources:
+	// 1. https://data.cityofchicago.org/Transportation/Taxi-Trips/wrvz-psew
+	// 2. https://data.cityofchicago.org/Transportation/Transportation-Network-Providers-Trips/m6dm-c72p
+
+	fmt.Println("GetBuildingPermits: Collecting Building Permits Data")
+
+	// Get your geocoder.ApiKey from here :
+	// https://developers.google.com/maps/documentation/geocoding/get-api-key?authuser=2
+
+	geocoder.ApiKey = "AIzaSyB-JwmMaEwb3yEomj66SnNlkA5GyKcRfWU"
+
+	drop_table := `drop table if exists building_permits`
+	_, err := db.Exec(drop_table)
+	if err != nil {
+		panic(err)
+	}
+
+	create_table := `CREATE TABLE IF NOT EXISTS "building_permits" (
+						"id"   SERIAL , 
+						"permit_id" VARCHAR(255) UNIQUE, 
+						"permit_issue_date" DATE, 
+						"community_area" INT, 
+						PRIMARY KEY ("id") 
+					);`
+
+	_, _err := db.Exec(create_table)
+	if _err != nil {
+		panic(_err)
+	}
+
+	// While doing unit-testing keep the limit value to 500
+	// later you could change it to 1000, 2000, 10,000, etc.
+	fmt.Println("Grabbing data from Chicago Data...")
+	var url = "https://data.cityofchicago.org/resource/ydr8-5enu.json"
+
+	res, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting JSON unmarshalling...")
+	body, _ := ioutil.ReadAll(res.Body)
+	var building_permits_list BuildingPermitsJsonRecords
+	json.Unmarshal(body, &building_permits_list)
+	fmt.Println("JSON unmarshalling done...")
+	fmt.Println("Now unpacking JSON and inserting into db... ")
+	for i := 0; i < len(building_permits_list); i++ {
+
+		// We will execute definsive coding to check for messy/dirty/missing data values
+		// Any record that has messy/dirty/missing data we don't enter it in the data lake/table
+
+		permit_id := building_permits_list[i].Permit_id
+		if permit_id == "" {
+			continue
+		}
+
+		// if trip start/end timestamp doesn't have the length of 23 chars in the format "0000-00-00T00:00:00.000"
+		// skip this record
+
+		// get Trip_start_timestamp
+		permit_issue_date := building_permits_list[i].Permit_issue_date
+		if permit_issue_date == "" {
+			continue
+		}
+
+		// get Trip_end_timestamp
+		community_area := building_permits_list[i].Community_area
+		if community_area == "" {
+			continue
+		}
+
+		sql := `INSERT INTO building_permits ("permit_id", "permit_issue_date", "community_area") values($1, $2, $3)`
+
+		_, err = db.Exec(
+			sql,
+			permit_id,
+			permit_issue_date,
+			community_area,
+		)
+
+		if err != nil {
+			panic(err)
+		}
+
+	}
+	fmt.Println("== Done with inserting Building Permits ==")
+
+}
+
+func GetTaxiTrips(db *sql.DB) {
+
+	// This function is NOT complete
+	// It provides code-snippets for the data source: https://data.cityofchicago.org/Transportation/Taxi-Trips/wrvz-psew
+	// You need to complete the implmentation and add the data source: https://data.cityofchicago.org/Transportation/Transportation-Network-Providers-Trips/m6dm-c72p
+
+	// Data Collection needed from two data sources:
+	// 1. https://data.cityofchicago.org/Transportation/Taxi-Trips/wrvz-psew
+	// 2. https://data.cityofchicago.org/Transportation/Transportation-Network-Providers-Trips/m6dm-c72p
+
+	fmt.Println("GetTaxiTrips: Collecting Taxi Trips Data")
+
+	// Get your geocoder.ApiKey from here :
+	// https://developers.google.com/maps/documentation/geocoding/get-api-key?authuser=2
+
+	geocoder.ApiKey = "AIzaSyB-JwmMaEwb3yEomj66SnNlkA5GyKcRfWU"
+
+	drop_table := `drop table if exists taxi_trips`
+	_, err := db.Exec(drop_table)
+	if err != nil {
+		panic(err)
+	}
+
+	create_table := `CREATE TABLE IF NOT EXISTS "taxi_trips" (
+						"id"   SERIAL , 
+						"trip_id" VARCHAR(255) UNIQUE, 
+						"trip_start_timestamp" TIMESTAMP WITH TIME ZONE, 
+						"trip_end_timestamp" TIMESTAMP WITH TIME ZONE, 
+						"pickup_centroid_latitude" DOUBLE PRECISION, 
+						"pickup_centroid_longitude" DOUBLE PRECISION, 
+						"dropoff_centroid_latitude" DOUBLE PRECISION, 
+						"dropoff_centroid_longitude" DOUBLE PRECISION, 
+						"pickup_zip_code" VARCHAR(255), 
+						"dropoff_zip_code" VARCHAR(255), 
+						PRIMARY KEY ("id") 
+					);`
+
+	_, _err := db.Exec(create_table)
+	if _err != nil {
+		panic(_err)
+	}
+
+	// While doing unit-testing keep the limit value to 500
+	// later you could change it to 1000, 2000, 10,000, etc.
+	fmt.Println("Grabbing data from Chicago Data...")
+	var url = "https://data.cityofchicago.org/resource/wrvz-psew.json?$limit=500"
+
+	res, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting JSON unmarshalling...")
+	body, _ := ioutil.ReadAll(res.Body)
+	var taxi_trips_list TaxiTripsJsonRecords
+	json.Unmarshal(body, &taxi_trips_list)
+	fmt.Println("JSON unmarshalling done...")
+	fmt.Println("Now unpacking JSON and inserting into db... ")
+	for i := 0; i < len(taxi_trips_list); i++ {
+
+		// We will execute definsive coding to check for messy/dirty/missing data values
+		// Any record that has messy/dirty/missing data we don't enter it in the data lake/table
+
+		trip_id := taxi_trips_list[i].Trip_id
+		if trip_id == "" {
+			continue
+		}
+
+		// if trip start/end timestamp doesn't have the length of 23 chars in the format "0000-00-00T00:00:00.000"
+		// skip this record
+
+		// get Trip_start_timestamp
+		trip_start_timestamp := taxi_trips_list[i].Trip_start_timestamp
+		if len(trip_start_timestamp) < 23 {
+			continue
+		}
+
+		// get Trip_end_timestamp
+		trip_end_timestamp := taxi_trips_list[i].Trip_end_timestamp
+		if len(trip_end_timestamp) < 23 {
+			continue
+		}
+
+		pickup_centroid_latitude := taxi_trips_list[i].Pickup_centroid_latitude
+
+		if pickup_centroid_latitude == "" {
+			continue
+		}
+
+		pickup_centroid_longitude := taxi_trips_list[i].Pickup_centroid_longitude
+		//pickup_centroid_longitude := taxi_trips_list[i].PICKUP_LONG
+
+		if pickup_centroid_longitude == "" {
+			continue
+		}
+
+		dropoff_centroid_latitude := taxi_trips_list[i].Dropoff_centroid_latitude
+		//dropoff_centroid_latitude := taxi_trips_list[i].DROPOFF_LAT
+
+		if dropoff_centroid_latitude == "" {
+			continue
+		}
+
+		dropoff_centroid_longitude := taxi_trips_list[i].Dropoff_centroid_longitude
+		//dropoff_centroid_longitude := taxi_trips_list[i].DROPOFF_LONG
+
+		if dropoff_centroid_longitude == "" {
+			continue
+		}
+
+		// Using pickup_centroid_latitude and pickup_centroid_longitude in geocoder.GeocodingReverse
+		// we could find the pickup zip-code
+
+		pickup_centroid_latitude_float, _ := strconv.ParseFloat(pickup_centroid_latitude, 64)
+		pickup_centroid_longitude_float, _ := strconv.ParseFloat(pickup_centroid_longitude, 64)
+		pickup_location := geocoder.Location{
+			Latitude:  pickup_centroid_latitude_float,
+			Longitude: pickup_centroid_longitude_float,
+		}
+
+		pickup_address_list, _ := geocoder.GeocodingReverse(pickup_location)
+		pickup_address := pickup_address_list[0]
+		pickup_zip_code := pickup_address.PostalCode
+
+		// Using dropoff_centroid_latitude and dropoff_centroid_longitude in geocoder.GeocodingReverse
+		// we could find the dropoff zip-code
+
+		dropoff_centroid_latitude_float, _ := strconv.ParseFloat(dropoff_centroid_latitude, 64)
+		dropoff_centroid_longitude_float, _ := strconv.ParseFloat(dropoff_centroid_longitude, 64)
+
+		dropoff_location := geocoder.Location{
+			Latitude:  dropoff_centroid_latitude_float,
+			Longitude: dropoff_centroid_longitude_float,
+		}
+
+		dropoff_address_list, _ := geocoder.GeocodingReverse(dropoff_location)
+		dropoff_address := dropoff_address_list[0]
+		dropoff_zip_code := dropoff_address.PostalCode
+
+		sql := `INSERT INTO taxi_trips ("trip_id", "trip_start_timestamp", "trip_end_timestamp", "pickup_centroid_latitude", "pickup_centroid_longitude", "dropoff_centroid_latitude", "dropoff_centroid_longitude", "pickup_zip_code", 
+			"dropoff_zip_code") values($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+
+		_, err = db.Exec(
+			sql,
+			trip_id,
+			trip_start_timestamp,
+			trip_end_timestamp,
+			pickup_centroid_latitude,
+			pickup_centroid_longitude,
+			dropoff_centroid_latitude,
+			dropoff_centroid_longitude,
+			pickup_zip_code,
+			dropoff_zip_code)
+
+		if err != nil {
+			panic(err)
+		}
+
+	}
+	fmt.Println("== Done with Taxi Trips ==")
+
+}
+
+func GetTransportTrips(db *sql.DB) {
+
+	// This function is NOT complete
+	// It provides code-snippets for the data source: https://data.cityofchicago.org/Transportation/Taxi-Trips/wrvz-psew
+	// You need to complete the implmentation and add the data source: https://data.cityofchicago.org/Transportation/Transportation-Network-Providers-Trips/m6dm-c72p
+
+	// Data Collection needed from two data sources:
+	// 1. https://data.cityofchicago.org/Transportation/Taxi-Trips/wrvz-psew
+	// 2. https://data.cityofchicago.org/Transportation/Transportation-Network-Providers-Trips/m6dm-c72p
+
+	fmt.Println("GetTransportProviders: Collecting Transport Trips Data")
+
+	// Get your geocoder.ApiKey from here :
+	// https://developers.google.com/maps/documentation/geocoding/get-api-key?authuser=2
+
+	geocoder.ApiKey = "AIzaSyB-JwmMaEwb3yEomj66SnNlkA5GyKcRfWU"
+
+	drop_table := `drop table if exists transport_trips`
+	_, err := db.Exec(drop_table)
+	if err != nil {
+		panic(err)
+	}
+
+	create_table := `CREATE TABLE IF NOT EXISTS "transport_trips" (
+						"id"   SERIAL , 
+						"trip_id" VARCHAR(255) UNIQUE, 
+						"trip_start_timestamp" TIMESTAMP WITH TIME ZONE, 
+						"trip_end_timestamp" TIMESTAMP WITH TIME ZONE, 
+						"pickup_centroid_latitude" DOUBLE PRECISION, 
+						"pickup_centroid_longitude" DOUBLE PRECISION, 
+						"dropoff_centroid_latitude" DOUBLE PRECISION, 
+						"dropoff_centroid_longitude" DOUBLE PRECISION, 
+						"pickup_zip_code" VARCHAR(255), 
+						"dropoff_zip_code" VARCHAR(255), 
+						PRIMARY KEY ("id") 
+					);`
+
+	_, _err := db.Exec(create_table)
+	if _err != nil {
+		panic(_err)
+	}
+
+	// While doing unit-testing keep the limit value to 500
+	// later you could change it to 1000, 2000, 10,000, etc.
+	fmt.Println("Grabbing data from Chicago Data...")
+	var url = "https://data.cityofchicago.org/resource/m6dm-c72p.json?$limit=500"
+
+	res, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting JSON unmarshalling...")
+	body, _ := ioutil.ReadAll(res.Body)
+	var transport_trips_list TransportTripsJsonRecords
+	json.Unmarshal(body, &transport_trips_list)
+	fmt.Println("JSON unmarshalling done...")
+	fmt.Println("Now unpacking JSON and inserting into db... ")
+	for i := 0; i < len(transport_trips_list); i++ {
+
+		// We will execute definsive coding to check for messy/dirty/missing data values
+		// Any record that has messy/dirty/missing data we don't enter it in the data lake/table
+
+		trip_id := transport_trips_list[i].Trip_id
+		if trip_id == "" {
+			continue
+		}
+
+		// if trip start/end timestamp doesn't have the length of 23 chars in the format "0000-00-00T00:00:00.000"
+		// skip this record
+
+		// get Trip_start_timestamp
+		trip_start_timestamp := transport_trips_list[i].Trip_start_timestamp
+		if len(trip_start_timestamp) < 23 {
+			continue
+		}
+
+		// get Trip_end_timestamp
+		trip_end_timestamp := transport_trips_list[i].Trip_end_timestamp
+		if len(trip_end_timestamp) < 23 {
+			continue
+		}
+
+		pickup_centroid_latitude := transport_trips_list[i].Pickup_centroid_latitude
+
+		if pickup_centroid_latitude == "" {
+			continue
+		}
+
+		pickup_centroid_longitude := transport_trips_list[i].Pickup_centroid_longitude
+		//pickup_centroid_longitude := taxi_trips_list[i].PICKUP_LONG
+
+		if pickup_centroid_longitude == "" {
+			continue
+		}
+
+		dropoff_centroid_latitude := transport_trips_list[i].Dropoff_centroid_latitude
+		//dropoff_centroid_latitude := taxi_trips_list[i].DROPOFF_LAT
+
+		if dropoff_centroid_latitude == "" {
+			continue
+		}
+
+		dropoff_centroid_longitude := transport_trips_list[i].Dropoff_centroid_longitude
+		//dropoff_centroid_longitude := taxi_trips_list[i].DROPOFF_LONG
+
+		if dropoff_centroid_longitude == "" {
+			continue
+		}
+
+		// Using pickup_centroid_latitude and pickup_centroid_longitude in geocoder.GeocodingReverse
+		// we could find the pickup zip-code
+
+		pickup_centroid_latitude_float, _ := strconv.ParseFloat(pickup_centroid_latitude, 64)
+		pickup_centroid_longitude_float, _ := strconv.ParseFloat(pickup_centroid_longitude, 64)
+		pickup_location := geocoder.Location{
+			Latitude:  pickup_centroid_latitude_float,
+			Longitude: pickup_centroid_longitude_float,
+		}
+
+		pickup_address_list, _ := geocoder.GeocodingReverse(pickup_location)
+		pickup_address := pickup_address_list[0]
+		pickup_zip_code := pickup_address.PostalCode
+
+		// Using dropoff_centroid_latitude and dropoff_centroid_longitude in geocoder.GeocodingReverse
+		// we could find the dropoff zip-code
+
+		dropoff_centroid_latitude_float, _ := strconv.ParseFloat(dropoff_centroid_latitude, 64)
+		dropoff_centroid_longitude_float, _ := strconv.ParseFloat(dropoff_centroid_longitude, 64)
+
+		dropoff_location := geocoder.Location{
+			Latitude:  dropoff_centroid_latitude_float,
+			Longitude: dropoff_centroid_longitude_float,
+		}
+
+		dropoff_address_list, _ := geocoder.GeocodingReverse(dropoff_location)
+		dropoff_address := dropoff_address_list[0]
+		dropoff_zip_code := dropoff_address.PostalCode
+
+		sql := `INSERT INTO transport_trips ("trip_id", "trip_start_timestamp", "trip_end_timestamp", "pickup_centroid_latitude", "pickup_centroid_longitude", "dropoff_centroid_latitude", "dropoff_centroid_longitude", "pickup_zip_code", 
+			"dropoff_zip_code") values($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+
+		_, err = db.Exec(
+			sql,
+			trip_id,
+			trip_start_timestamp,
+			trip_end_timestamp,
+			pickup_centroid_latitude,
+			pickup_centroid_longitude,
+			dropoff_centroid_latitude,
+			dropoff_centroid_longitude,
+			pickup_zip_code,
+			dropoff_zip_code)
+
+		if err != nil {
+			panic(err)
+		}
+
+	}
+	fmt.Println("== Done with Transport Trips ==")
+
+}
+
+func GetUnemploymentData(db *sql.DB) {
+
+	// This function is NOT complete
+	// It provides code-snippets for the data source: https://data.cityofchicago.org/Transportation/Taxi-Trips/wrvz-psew
+	// You need to complete the implmentation and add the data source: https://data.cityofchicago.org/Transportation/Transportation-Network-Providers-Trips/m6dm-c72p
+
+	// Data Collection needed from two data sources:
+	// 1. https://data.cityofchicago.org/Transportation/Taxi-Trips/wrvz-psew
+	// 2. https://data.cityofchicago.org/Transportation/Transportation-Network-Providers-Trips/m6dm-c72p
+
+	fmt.Println("GetUnemploymentData: Grabbing Unemployment Data")
+
+	// Get your geocoder.ApiKey from here :
+	// https://developers.google.com/maps/documentation/geocoding/get-api-key?authuser=2
+
+	geocoder.ApiKey = "AIzaSyB-JwmMaEwb3yEomj66SnNlkA5GyKcRfWU"
+
+	drop_table := `drop table if exists unemployment_data`
+	_, err := db.Exec(drop_table)
+	if err != nil {
+		panic(err)
+	}
+
+	create_table := `CREATE TABLE IF NOT EXISTS unemployment_data (
+						"id"   SERIAL , 
+						"community_area" VARCHAR(255), 
+						"unemployment_rate" FLOAT,
+						PRIMARY KEY ("id") 
+					);`
+
+	_, _err := db.Exec(create_table)
+	if _err != nil {
+		panic(_err)
+	}
+
+	// While doing unit-testing keep the limit value to 500
+	// later you could change it to 1000, 2000, 10,000, etc.
+	fmt.Println("Grabbing data from Chicago Data...")
+	var url = "https://data.cityofchicago.org/resource/iqnk-2tcu.json"
+
+	res, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting JSON unmarshalling...")
+	body, _ := ioutil.ReadAll(res.Body)
+	var unemployment_list UnemploymentJsonRecords
+	json.Unmarshal(body, &unemployment_list)
+	fmt.Println("JSON unmarshalling done...")
+	fmt.Println("Now unpacking JSON and inserting into db... ")
+	for i := 0; i < len(unemployment_list); i++ {
+
+		// We will execute definsive coding to check for messy/dirty/missing data values
+		// Any record that has messy/dirty/missing data we don't enter it in the data lake/table
+
+		community_area := unemployment_list[i].Community_area
+		if community_area == "" {
+			continue
+		}
+
+		unemployment_rate := unemployment_list[i].Unemployment
+		if unemployment_rate == "" {
+			continue
+		}
+
+		sql := `INSERT INTO unemployment_data ("community_area", "unemployment_rate"
+			) values($1, $2)`
+
+		_, err = db.Exec(
+			sql,
+			community_area,
+			unemployment_rate)
+
+		if err != nil {
+			panic(err)
+		}
+
+	}
+	fmt.Println("== Done with Unemployment Data ==")
 
 }
