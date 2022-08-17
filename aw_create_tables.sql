26073456a4b7ee1de55c81bb7758e0ee3d9d1836
Andrew Whelan <andrewmwhelan9@gmail.com>
2022-08-17 12:52:45 +0000

    adding tables

--- aw_create_tables.sql
+++ aw_create_tables.sql
@@ -16,3 +16,25 @@ DROP TABLE IF EXISTS MSDSCourseCatalog;
 
 SELECT * FROM MSDSCourseCatalog;
 */
+
+CREATE TABLE locations (
+    neighborhood VARCHAR(100),
+    community_area VARCHAR(100),
+    zip_code VARCHAR(100)
+);
+
+INSERT INTO locations (neighborhood,community_area,zip_code) VALUES 
+    ('Rogers Park','1','60626'),
+    ('Rogers Park','1','60645'),
+    ('Rogers Park','1','60660'),
+    ('West Ridge','2','60626'),
+    ('West Ridge','2','60645'),
+    ('West Ridge','2','60659'),
+    ('West Ridge','2','60660'),
+    ('Uptown','3','60613'),
+    ('Uptown','3','60640'),
+    ('Uptown','3','60618')
+
+;
+
+SELECT * FROM locations
